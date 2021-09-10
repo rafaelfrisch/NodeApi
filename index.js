625fd28224062754f3ac15ac8e32ff98239335ac
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-09-10 18:17:23 -0300

    File upload

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const cors = require('cors');
+const multer = require('multer');
 
+const app = express()
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-  
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
 
 app.use(express.static('public'));
-
-//Serves all the request which includes /images in the url from Images folder
+app.use(cors());
 app.use('/images', express.static(__dirname + '/Images'));
+
+const upload = multer({
+    dest: './uploads/',
+});
+
+app.post('/upload', upload.array('file'), async (req, res) => {
+    res.send({
+      upload: true,
+      files: req.files,
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
